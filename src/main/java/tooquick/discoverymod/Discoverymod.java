ae4c95147ae1976a3dd829e803a667f08ca537d4
JamDoggie <JamDoggie3@gmail.com>
2023-05-21 19:48:33 -0400

    Initial commit

---
+++ src/main/java/tooquick/discoverymod/Discoverymod.java
@@ -0,0 +1,12 @@
+package tooquick.discoverymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Discoverymod implements ModInitializer
+{
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
